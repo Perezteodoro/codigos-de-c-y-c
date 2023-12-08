0b9985bdb411e3e8ac757b564e467631244df381
Perezteodoro <153297987+Perezteodoro@users.noreply.github.com>
2023-12-08 08:40:50 -0600

    Add files via upload

---
+++ Operaciones Basicas.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+	int n1,n2, suma=0, multiplicacion=0, division=0;
+	
+	cout<<"Inserte un digito:"; cin>>n1;
+	cout<<"Inserte otro digito:"; cin>>n2;
+	
+	suma=n1+n2;
+	resta=n1-n2;
+	multipliacion=n1*n2;
+	division=n1/n2;
+	
+	cout<<"\nLa suma es:"<<suma<<end1;
+	cout<<"La resta es:" <<resta<<end1;
+	cout<<"la multipicacion es:"<<multipliacion<<end1;
+	cout<<"la division es:" <<division<<end1;
+	
+	return 0;
+}
